a1d446ff77b35e5e526a31e3eec28f302e36d89d
Nasser Marafi <nasser.marafi@gmail.com>
2016-02-09 17:07:19 -0800

    includes the packages which were missing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='OpenSeesAPI',
-      version='0.1',
+      version='0.12',
       # install_requires=[
       # "numpy",
       # "matplotlib",
@@ -10,6 +10,6 @@ setup(name='OpenSeesAPI',
       url='http://github.com/nassermarafi/OpenSeesAPI',
       author='Nasser Marafi',
       author_email='marafi@uw.edu',
-      license='UW',
-      packages=['OpenSeesAPI'],
+      license='MIT',
+      packages=['OpenSeesAPI', 'OpenSeesAPI.Analysis', 'OpenSeesAPI.Model','test'],
       zip_safe=False)
