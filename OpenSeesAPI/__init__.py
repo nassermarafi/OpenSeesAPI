5e03454e0f8d33ae8f081a44a736e320971bb608
Nasser Marafi <nasser.marafi@gmail.com>
2016-08-28 11:34:14 -0700

    simplified callouts but with backwards compability
    we can delete line 6 once we don't want the backwards compatibility.

--- OpenSeesAPI/__init__.py
+++ OpenSeesAPI/__init__.py
@@ -1,11 +1,24 @@
 __author__ = 'marafi'
 
-# Import Classes
+# Import Modules with Classes
 from OpenSeesAPI import Analysis
-from OpenSeesAPI import Model
 from OpenSeesAPI import Database
+from OpenSeesAPI import Model # included for backwards compatibility
 from OpenSeesAPI import Output
 from OpenSeesAPI import TCL
 
-from OpenSeesAPI.OpenSees import OpenSees
+from OpenSeesAPI.Model import BasicBuilder
+from OpenSeesAPI.Model import Constraint
+from OpenSeesAPI.Model import Node
+from OpenSeesAPI.Model import Pattern
+from OpenSeesAPI.Model import TimeSeries
+from OpenSeesAPI.Model.Element import Element
+from OpenSeesAPI.Model.Element import GeomTransf
+from OpenSeesAPI.Model.Element import Material
+from OpenSeesAPI.Model.Element.Material import Section
+
+# Importing Classes
 from OpenSeesAPI.OpenSees import Executable
+from OpenSeesAPI.OpenSees import OpenSees
+
+
